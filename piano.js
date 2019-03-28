ccf8ae9bbfcf716687d3de21198aec3f66a2e468
Jose Horta <joshhortt@yahoo.com>
2019-03-28 18:46:14 +0000

    add transition end Event and moved script from html to js file

--- piano.js
+++ piano.js
@@ -1,15 +1,16 @@
-function playSound(event) {
-	const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
-	const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
-	if (!audio) return; //stops the function from running
-	audio.currentTime = 0; //rewinds to the start of audio
-	audio.play();
-	key.classList.add('playing');
-}
-function removeTransition(event) {
-	if (event.propertyName !== 'transform') return; //skip if not transform
-	this.classList.remove('playing');
-}
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition))
-window.addEventListener('keydown', playSound)
+
+  function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+  }
+  function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+  }
+  const keys = Array.from(document.querySelectorAll('.key'));
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  window.addEventListener('keydown', playSound);
